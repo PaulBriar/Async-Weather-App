d12c000ab55a7876adf99b37013ce86829658743
paulbriar <pbriar@live.com>
2018-12-02 13:13:53 -0400

    Refactored geolocation request to its own module

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
-const googleAPIKey = require('./APIs/apiKeys');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs.options({
     a: {
@@ -14,18 +13,10 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv;
 
-let userInput = encodeURIComponent(argv.a);
-
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userInput}&key=` + googleAPIKey,
-    json: true,
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find address.');   
-    } else if (body.status === 'OK') {
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}. Longitude: ${body.results[0].geometry.location.lng}.`); 
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
