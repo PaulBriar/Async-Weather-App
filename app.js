441acc4c4c7c7bf93e2e4997fe4540a5befa4045
paulbriar <pbriar@live.com>
2018-12-01 21:50:24 -0400

    Added dynamic user input and error handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
 const googleAPIKey = require('./APIs/apiKeys');
 
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true,
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+let userInput = encodeURIComponent(argv.a);
+
+
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=' + googleAPIKey,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userInput}&key=` + googleAPIKey,
     json: true,
 }, (error, response, body) => {
-    console.log(body);
-    
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find address.');   
+    } else if (body.status === 'OK') {
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}. Longitude: ${body.results[0].geometry.location.lng}.`); 
+    }
 });
